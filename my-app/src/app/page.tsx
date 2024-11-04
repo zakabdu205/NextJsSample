c2654df257ea1a1c6094f8093dd025e27ca2ed7d
zakabdu205 <zakabdu205@hotmail.com>
2024-11-04 14:21:05 +1030

    nextjs-tutorial

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,6 +1,10 @@
+"use client";
 import Image from "next/image";
+import React, { useState, useRef, useEffect } from "react";
 
 export default function Home() {
+  const [ssff, setSsff] = useState(0);
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -48,6 +52,8 @@ export default function Home() {
             Read our docs
           </a>
         </div>
+
+        {/* Add the ZoomableCanvas component here */}
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
