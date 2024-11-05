41a33aa16a579be7713ab2a2085ac0e323b9408c
zakabdu205 <zakabdu205@hotmail.com>
2024-11-05 11:46:46 +1030

    updates0.1

---
+++ my-app/src/component/canvas.tsx
@@ -0,0 +1,12 @@
+export type Test = {
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+};
+export type Test2 = {
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+};
